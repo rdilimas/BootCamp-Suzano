9f832261712a3f384fa1585622ced03d6ad98d83
rdilimas <rdilimas@gmail.com>
2025-08-12 01:31:26 -0300

    feat: Incluir descrição do desafio no fonte.

--- Explorando Sintaxe e Lógica em Python/controle_acesso.py
+++ Explorando Sintaxe e Lógica em Python/controle_acesso.py
@@ -1,3 +1,12 @@
+'''Controle de Acesso Inteligente
+Uma biblioteca está implementando um sistema automatizado para 
+liberar o acesso à área de livros raros. O sistema deve permitir a 
+entrada apenas para visitantes autorizados e com idade mínima de 18 anos. 
+Para isso, é necessário utilizar operadores lógicos, estruturas condicionais (if, else, else if) e 
+conceitos básicos como tipos primitivos e palavras-chave. Desenvolva um programa que verifique se o 
+visitante pode acessar a sala especial.
+'''
+
 def main():
     # Entrada de dados do usuário
     has_permission = input().lower() == "true"  
